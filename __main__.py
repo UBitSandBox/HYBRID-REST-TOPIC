e1552ca59d50f2d1e176f1096f7a1023a2ed31b5
Sébastien MEYER <sbmeyer68@gmail.com>
2020-02-26 13:38:53 +0100

    Correcting a path join that wasn't relative path

--- __main__.py
+++ __main__.py
@@ -2,24 +2,26 @@ import json2vec
 import time
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
+dir_path = os.path.dirname(os.path.realpath(__file__))  # Path where the module was loaded
+index_file_name = "my.index"
+test_lang = "de"
+json_path = "json/de/"
 
-if not os.path.exists(os.path.join(dir_path, "my.index")):
+if not os.path.exists(os.path.join(dir_path, index_file_name)):
     start = time.clock()
     IM = json2vec.IndexManager()
-    IM.set_dictionary('de')
+    IM.set_dictionary(test_lang)
     mid = time.clock()
-    path = os.path.join(dir_path, "/json/de/")
+    path = os.path.join(dir_path, json_path)
     files = os.listdir(path)
     for file in files:
         IM.add(path + file)
-    IM.save(os.path.join(dir_path, "my.index"))
+    IM.save(os.path.join(dir_path, index_file_name))
     end = time.clock()
     print("Loading dictionary in", mid - start, "seconds. Adding", IM.size(), "files to the index in", end - mid,
           "seconds")
 
-IM = json2vec.IndexManager(os.path.join(dir_path, "my.index"))
+IM = json2vec.IndexManager(os.path.join(dir_path, index_file_name))
 
 try:
     start = time.clock()
