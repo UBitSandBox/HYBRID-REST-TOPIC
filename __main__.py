32aa4fb608d56dd7bc5eea12f565d56d4844085d
Sébastien MEYER <sbmeyer68@gmail.com>
2020-03-12 16:13:36 +0100

    Changed accordingly to the new parametrisation options

--- __main__.py
+++ __main__.py
@@ -1,15 +1,16 @@
-import json2vec
+from IndexManager import IndexManager
 import time
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))  # Path where the module was loaded
-index_file_name = "my.index"
+index_relative_path = "saved_indexes/my.index"
 test_lang = "de"
-json_path = "json/de/"
+json_path = "json_files/de/"
 
-if not os.path.exists(os.path.join(dir_path, index_file_name)):
+if not os.path.exists(os.path.join(dir_path, index_relative_path)):
     start = time.clock()
-    IM = json2vec.IndexManager()
+    IM = IndexManager(n_clusters=5, cluster_weight_threshold=0.1, max_clustering_input_size=1000,
+                      cluster_affinity="euclidean", cluster_linkage="ward", pos_tags_to_keep=["NN"])
     IM.set_parameter("n_clusters", 3)
     IM.set_dictionary(test_lang)
     mid = time.clock()
@@ -17,14 +18,14 @@ if not os.path.exists(os.path.join(dir_path, index_file_name)):
     files = os.listdir(path)
     for file in files:
         IM.add(path + file)
-    IM.save(os.path.join(dir_path, index_file_name))
+    IM.save(os.path.join(dir_path, index_relative_path))
     end = time.clock()
-    print("Loading dictionary in", mid - start, "seconds. Adding", IM.size(), "files to the index in", end - mid,
+    print("Loading dictionary in", mid - start, "seconds. Adding", IM.get_size(), "files to the index in", end - mid,
           "seconds")
 
-IM = json2vec.IndexManager(os.path.join(dir_path, index_file_name))
+IM = IndexManager(path=os.path.join(dir_path, index_relative_path))
 start = time.clock()
-Distances, IDs = zip(*IM.search(2000, 10))
+Distances, IDs = zip(*IM.search(1000, 10))
 end = time.clock()
 print(Distances)
 print(IDs)
