006dc8c022ef22556b9b875213a5717fc52afb2f
Jérôme Marquis <j.marquis@bluewin.ch>
2020-07-06 13:12:10 +0200

    Change access token lifetime to 1 day (for testing)

--- vectoREST/vectoREST/settings.py
+++ vectoREST/vectoREST/settings.py
@@ -132,5 +132,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    #'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 }
