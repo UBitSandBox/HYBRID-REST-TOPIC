7e9a9417c898a0d28f3430f60d7ab0ed2bc65a11
MEYER Sébastien <35813232+Shiaroku@users.noreply.github.com>
2020-07-08 18:03:05 +0200

    Simplified a "if condition : return boolean"

--- vectoREST/api/permissions.py
+++ vectoREST/api/permissions.py
@@ -6,16 +6,11 @@ class ConfigRight(BasePermission):
     Right for endpoint /config
     """
     def has_permission(self, request, view):
-        if request.user and request.user.groups.filter(name='config'):
-            return True
-        return False
-
-
+        return bool(request.user and request.user.groups.filter(name='config'))
+    
 class VectorsRight(BasePermission):
     """
     Right for endpoint /vectors
     """
     def has_permission(self, request, view):
-        if request.user and request.user.groups.filter(name='vectors'):
-            return True
-        return False
+        return bool(request.user and request.user.groups.filter(name='vectors'))
