1e7dd067f1c03a7532d10d8742f5bfd71fe3edea
MEYER Sébastien <35813232+Shiaroku@users.noreply.github.com>
2020-07-08 17:37:11 +0200

    Code simplification

--- vectoREST/api/vectors/views.py
+++ vectoREST/api/vectors/views.py
@@ -6,7 +6,6 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .VectorGenerator import VectorGenerator
 from .apps import VectorsConfig
-
 from ..permissions import VectorsRight
 
 
@@ -30,38 +29,17 @@ class Vectors(APIView):
         """
 
         # Check if lang is correct
-        if lang != 'en' and \
-           lang != 'fr' and \
-           lang != 'de' and \
-           lang != 'it':
-            raise NotFound(detail="Language '" + lang + "' is not found")
-
-        # Retrieve the file
-        # if 'file' not in request.data:
-        #     raise ParseError("A file is required")
-        # file = request.data['file']
+        list_of_supported_lang = ['en','fr','de','it']
+        
+        if lang not in list_of_supported_lang
+            raise NotFound(detail="Language '{lang}' is not found. Try one of the following : {suggestions}".format(lang=lang, suggestions = ", ".join(list_of_supported_lang)))
 
         if 'content' not in request.data:
             raise ParseError("Content is required")
 
         content = request.data['content']
-
-        # TODO: Do something with the content...
         dense_vector = VectorsConfig.vector_generator.doc2vec(document=content)
-
-
-
         dict_format_response = dict(zip(range(len(dense_vector)), map(float,dense_vector)))
-
-        print(dict_format_response)
-
-        # dense_vector = {
-        #     "0": 0.4433,
-        #     "1": 0.2244,
-        #     "2": 0.2345
-        # }
-
         response = dict(lang=lang, dense_vector=dict_format_response)
-        print(response)
-
+        
         return JsonResponse(response, safe=True)
